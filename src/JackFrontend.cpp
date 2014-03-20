2764449d9d72ee45c0a412a1f5d17d872a74a9e0
Jan Milik <milikjan@fit.cvut.cz>
2014-03-20 21:35:26 +0100

    Add logging message when sending MIDI message.

--- src/JackFrontend.cpp
+++ src/JackFrontend.cpp
@@ -42,6 +42,8 @@ int JackFrontend::onJackInput(jack_nframes_t nframes, void *arg)
 				string *msg = self->midiQueue_.front();
 				self->midiQueue_.pop_front();
 				
+				LOG_INFO("JackFrontend: Sending MIDI message: " << msg);
+				
 				unsigned char *buffer = jack_midi_event_reserve(
 					midiPortBuffer,
 					0,
